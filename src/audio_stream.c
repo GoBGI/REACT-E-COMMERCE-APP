7870ce2e806d79260b8af5b4e1b459db028c82d4
GoBGI <88916280+GoBGI@users.noreply.github.com>
2020-04-22 07:34:37 +0000

    Add vagrant

--- src/audio_stream.c
+++ src/audio_stream.c
@@ -83,3 +83,59 @@ struct AudioStream *audio_stream_open(const struct AudioStreamOptions *options) 
     if (avcodec_parameters_to_context(self->dec_ctx, self->in_stream->codecpar)) {
         lav_error("avcodec_parameters_to_context", result);
         goto fail;
+    }
+
+    if (avcodec_open2(self->dec_ctx, self->decoder, NULL)) {
+        lav_error("avcodec_open2", result);
+        goto fail;
+    }
+
+    if (!self->dec_ctx->channel_layout) {
+        self->dec_ctx->channel_layout = av_get_default_channel_layout(self->dec_ctx->channels);
+    }
+
+    if (options->start > 0) {
+        int64_t seek_pos = options->start / av_q2d(self->in_stream->time_base);
+        result = av_seek_frame(self->in_ctx, 0, seek_pos, 0);
+        if (result < 0) {
+            lav_error("av_seek_frame", result);
+            goto fail;
+        }
+    }
+
+    if (options->length > 0) {
+        self->end_pts = (options->start + options->length) / av_q2d(self->in_stream->time_base);
+    }
+
+    av_dump_format(self->in_ctx, 0, options->path, 0);
+
+    self->out_ctx = avformat_alloc_context();
+
+    self->out_ctx->oformat = av_guess_format(options->target_codec, NULL, NULL);
+    if (!self->out_ctx->oformat) {
+        lav_error("av_guess_format", 0);
+        goto fail;
+    }
+
+    self->out_stream = avformat_new_stream(self->out_ctx, NULL);
+    if (!self->out_stream) {
+        lav_error("avformat_new_stream", 0);
+    }
+
+    // TODO copy metadata
+
+    self->encoder = avcodec_find_encoder(self->out_ctx->oformat->audio_codec);
+    if (!self->encoder) {
+        lav_error("avcodec_find_encoder", 0);
+        goto fail;
+    }
+
+    self->enc_ctx = avcodec_alloc_context3(self->encoder);
+
+    self->enc_ctx->sample_fmt = find_sample_fmt(
+        self->dec_ctx->sample_fmt,
+        self->encoder->sample_fmts);
+    self->enc_ctx->sample_rate = find_sample_rate(
+        self->dec_ctx->sample_rate,
+        self->encoder->supported_samplerates);
+    self->enc_ctx->channel_layout = self->dec_ctx->channel_layout;
