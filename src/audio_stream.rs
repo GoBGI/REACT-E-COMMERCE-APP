d0d355157254166c49b1c52a8c45b387da8fc85d
GoBGI <88916280+GoBGI@users.noreply.github.com>
2020-05-14 14:45:10 +0000

    Updated Vietnamese translation

--- src/audio_stream.rs
+++ src/audio_stream.rs
@@ -9,4 +9,5 @@ use tokio::sync::mpsc::Sender;
 
 use crate::musicd_c;
 
-extern "C" fn stream_c_callback(opaque: *
+extern "C" fn stream_c_callback(opaque: *const c_void, data: *const u8, len: c_int) -> c_int {
+  
