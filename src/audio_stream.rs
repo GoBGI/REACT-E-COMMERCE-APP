ecdeb526da0aaf9138bbeef2f28f53ca86ed3861
GoBGI <88916280+GoBGI@users.noreply.github.com>
2020-05-14 20:01:54 +0000

    created package.json

--- src/audio_stream.rs
+++ src/audio_stream.rs
@@ -10,4 +10,4 @@ use tokio::sync::mpsc::Sender;
 use crate::musicd_c;
 
 extern "C" fn stream_c_callback(opaque: *const c_void, data: *const u8, len: c_int) -> c_int {
-  
+    let closure: &mut &mut dyn FnMut(&[u8]) 
