84502b7512d641c17283fa9599b4f5d99d29d994
GoBGI <88916280+GoBGI@users.noreply.github.com>
2020-05-16 07:44:21 +0000

    reversed

--- src/audio_stream.rs
+++ src/audio_stream.rs
@@ -10,4 +10,5 @@ use tokio::sync::mpsc::Sender;
 use crate::musicd_c;
 
 extern "C" fn stream_c_callback(opaque: *const c_void, data: *const u8, len: c_int) -> c_int {
-    let closure: &mut &mut dyn FnMut(&[u8]) 
+    let closure: &mut &mut dyn FnMut(&[u8]) -> usize =
+        unsafe { &mut *(opaque as *mut &mut
