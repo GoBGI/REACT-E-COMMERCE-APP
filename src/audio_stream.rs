cd39b06f5f05cac199234a895bb89edff33f0cea
GoBGI <88916280+GoBGI@users.noreply.github.com>
2020-06-13 05:58:39 +0000

    Bugfix

--- src/audio_stream.rs
+++ src/audio_stream.rs
@@ -71,4 +71,9 @@ impl AudioStream {
 
         unsafe {
             musicd_c::audio_stream_next(self.stream, cb as *mut _ as *mut c_void, stream_c_callback)
-                >
+                > 0
+        }
+    }
+
+    pub async fn execute(
+       
