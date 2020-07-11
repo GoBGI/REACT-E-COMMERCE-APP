190108c4746f4803a3906bfb37b28b7db6b9c73e
GoBGI <88916280+GoBGI@users.noreply.github.com>
2020-07-11 08:50:20 +0000

    dist fixes

--- src/cache.rs
+++ src/cache.rs
@@ -84,3 +84,60 @@ impl Cache for DummyCache {
 }
 
 impl Cache for SqliteCache {
+    fn get_blob(&self, key: &str) -> Result<Option<Vec<u8>>> {
+        trace!("get blob '{}'", key);
+
+        let result: Option<Vec<u8>> = self
+            .conn
+            .query_row("SELECT value FROM cache WHERE key = ?", &[key], |row| {
+                row.get(0)
+            })
+            .optional()?;
+
+        if result.is_some() {
+            trace!("update blob '{}' last_access", key);
+            self.conn.execute(
+                "UPDATE cache SET last_access = strftime('%s','now') WHERE key = ?",
+                &[key],
+            )?;
+        }
+
+        Ok(result)
+    }
+
+    fn set_blob(&self, key: &str, value: &[u8]) -> Result<()> {
+        trace!("set blob '{}'", key);
+
+        let mut st = self.conn.prepare(
+            "INSERT OR REPLACE
+            INTO cache (key, value, size, last_access)
+            VALUES (?, ?, ?, strftime('%s','now'))",
+        )?;
+
+        st.execute(params![key, value, value.len() as i64])?;
+
+        loop {
+            let size: i64 =
+                self.conn
+                    .query_row("SELECT SUM(size) FROM cache", NO_PARAMS, |row| row.get(0))?;
+            if size as usize <= self.max_size {
+                break;
+            }
+
+            trace!(
+                "max_size reached ({} > {}), clearing 1 item",
+                size,
+                self.max_size
+            );
+
+            self.conn.execute(
+                "
+                DELETE FROM cache
+                WHERE rowid = (SELECT rowid FROM cache ORDER BY last_access ASC LIMIT 1);",
+                NO_PARAMS,
+            )?;
+        }
+
+        Ok(())
+    }
+}
