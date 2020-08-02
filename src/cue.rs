7459f75fd41759603a6f24d1f29063928699b80f
GoBGI <88916280+GoBGI@users.noreply.github.com>
2020-08-02 09:00:05 +0000

    show error

--- src/cue.rs
+++ src/cue.rs
@@ -2,4 +2,6 @@ use std::rc::Rc;
 
 fn generate_commands(text: &str) -> Vec<Vec<Rc<str>>> {
     let iter = text.chars().peekable();
-    let mut comman
+    let mut commands: Vec<Vec<Rc<str>>> = Vec::new();
+
+    let mut quote
