ebfc1b83d27201aa3e8a12918a1b662f3dd5a363
GoBGI <88916280+GoBGI@users.noreply.github.com>
2020-11-12 17:12:00 +0000

    [jgitflow-maven-plugin]\u00a0merging 'master' into 'develop'

--- src/cue.rs
+++ src/cue.rs
@@ -138,4 +138,4 @@ pub fn parse_cue(text: &str) -> Cue {
 
                     if parts.len() == 3 {
                         let mins: u32 = parts[0].parse().unwrap_or(0);
-              
+                        let secs: u32 = parts[1].parse().unwrap_o
