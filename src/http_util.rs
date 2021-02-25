5f78fa508bc51f21f1ddf7a4bb70e734e52d9135
GoBGI <88916280+GoBGI@users.noreply.github.com>
2021-02-25 20:55:46 +0000

    drm/i915: always use RPNSWREQ for turbo change requests

--- src/http_util.rs
+++ src/http_util.rs
@@ -3,4 +3,4 @@ use std::collections::{BTreeMap, HashMap};
 use hyper::header::ToStrError;
 use hyper::HeaderMap;
 
-pub fn parse_cookies(headers: &Heade
+pub fn parse_cookies(headers: &HeaderMap) -> Result<HashMap<String, String>, T
