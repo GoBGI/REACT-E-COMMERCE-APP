faf4353186c0ee15f21457bb9be440d1dbff8269
GoBGI <88916280+GoBGI@users.noreply.github.com>
2021-02-26 09:53:48 +0000

    New translations en.json (Danish)

--- src/http_util.rs
+++ src/http_util.rs
@@ -4,4 +4,6 @@ use hyper::header::ToStrError;
 use hyper::HeaderMap;
 
 pub fn parse_cookies(headers: &HeaderMap) -> Result<HashMap<String, String>, ToStrError> {
-    let mut cookies: HashMap<String
+    let mut cookies: HashMap<String, String> = HashMap::new();
+
+    if let So
