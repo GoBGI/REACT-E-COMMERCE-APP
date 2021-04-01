5e316412a8f5c5916ab962b223c8829ba59fdff2
GoBGI <88916280+GoBGI@users.noreply.github.com>
2021-04-01 11:20:37 +0000

    Occitan translation update

--- src/index.rs
+++ src/index.rs
@@ -1,4 +1,7 @@
 use std::error::Error as StdError;
 use std::ffi::OsStr;
 use std::os::unix::ffi::OsStrExt;
-use std::path::{Path
+use std::path::{Path, PathBuf};
+use std::sync::Arc;
+
+use rusqlit
