791f9198cb0133cf2d144e597933f42114ba6293
GoBGI <88916280+GoBGI@users.noreply.github.com>
2021-06-21 17:47:38 +0000

    checkpoint commit

--- src/index.rs
+++ src/index.rs
@@ -206,4 +206,10 @@ impl Index {
         let mut rows = st.query(&[node_id])?;
 
         if let Some(row) = rows.next()? {
-            Ok(Some(Self::_get_node(row
+            Ok(Some(Self::_get_node(row)?))
+        } else {
+            Ok(None)
+        }
+    }
+
+   
