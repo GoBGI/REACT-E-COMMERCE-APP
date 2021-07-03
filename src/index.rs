137f920bf2a4e26137ffb87e154638358bdeea9c
GoBGI <88916280+GoBGI@users.noreply.github.com>
2021-07-03 06:10:45 +0000

    Cleanup old code

--- src/index.rs
+++ src/index.rs
@@ -227,4 +227,5 @@ impl Index {
             None => "
                 SELECT node_id, node_type, parent_id, master_id, name, path, modified
                 FROM Node
-     
+                WHERE name = ? AND parent_id IS NULL",
+    
