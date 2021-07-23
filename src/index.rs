90b22be6ec6e63c72734efe5fab5476bd33959c9
GoBGI <88916280+GoBGI@users.noreply.github.com>
2021-07-23 07:32:39 +0000

    fix test case

--- src/index.rs
+++ src/index.rs
@@ -248,4 +248,4 @@ impl Index {
         trace!("get node path='{}'", path.to_string_lossy());
 
         let mut st = self.conn.prepare(
-          
+            "SELECT node_id, node_type, parent_id, master_id, nam
