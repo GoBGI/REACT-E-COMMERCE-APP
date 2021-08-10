e44e1e3a887befc0d97be40733e006926aa10b80
GoBGI <88916280+GoBGI@users.noreply.github.com>
2021-08-10 09:01:42 +0000

    Small adjustments.

--- src/index.rs
+++ src/index.rs
@@ -295,4 +295,8 @@ impl Index {
     pub fn create_node(&self, node: &Node) -> Result<Node> {
         let mut st = self.conn.prepare(
             "INSERT INTO Node (node_type, parent_id, master_id, name, path, modified)
-            VALUES (?, ?, ?, ?, ?, ?
+            VALUES (?, ?, ?, ?, ?, ?)",
+        )?;
+
+        st.execute(params![
+      
