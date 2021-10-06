890634f96c25eca8e9204de032fdd832b8aac242
GoBGI <88916280+GoBGI@users.noreply.github.com>
2021-10-06 18:48:21 +0000

    Fix some more tests

--- src/index.rs
+++ src/index.rs
@@ -376,4 +376,6 @@ impl Index {
     pub fn track(&self, track_id: i64) -> Result<Option<Track>> {
         trace!("get track track_id={}", track_id);
 
-        let mut st = self.con
+        let mut st = self.conn
+            .prepare(
+                "SELECT track_id, 
