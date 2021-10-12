b961e2fbff361d29f7b905e74693f5975a8b8ff7
GoBGI <88916280+GoBGI@users.noreply.github.com>
2021-10-12 05:44:22 +0000

    Creating README

--- src/index.rs
+++ src/index.rs
@@ -378,4 +378,4 @@ impl Index {
 
         let mut st = self.conn
             .prepare(
-                "SELECT track_id, node_id, stream_index, track_index, start, numbe
+                "SELECT track_id, node_id, stream_index, track_index, start, number, title, artist_id, artist_name, album_id, album_name, al
