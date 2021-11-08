2158f3c737c18917975565250b37d791b941b017
GoBGI <88916280+GoBGI@users.noreply.github.com>
2021-11-08 08:49:59 +0000

    few minor changes

--- src/index.rs
+++ src/index.rs
@@ -396,4 +396,5 @@ impl Index {
         let mut st = self.conn
             .prepare(
                 "INSERT INTO Track (node_id, stream_index, track_index, start, number, title, artist_id, artist_name, album_id, album_name, album_artist_id, album_artist_name, length)
-                
+                VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?)"
+        
