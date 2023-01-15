1ff51ce669b744a5eb21d3b0713b15ccb07ddece
GoBGI <88916280+GoBGI@users.noreply.github.com>
2023-01-15 19:36:39 +0000

    bump inigo

--- src/media.c
+++ src/media.c
@@ -47,3 +47,46 @@ struct MediaInfo *media_info_from_path(const char *path) {
     }
 
     if (fmt->audio_codec == AV_CODEC_ID_NONE && fmt->video_codec == AV_CODEC_ID_NONE) {
+        return NULL;
+    }
+
+    AVFormatContext *avctx = NULL;
+    if (avformat_open_input(&avctx, path, NULL, NULL) < 0) {
+        return NULL;
+    }
+
+    if (avctx->nb_streams < 1 || avctx->duration < 1) {
+        if (avformat_find_stream_info(avctx, NULL) < 0) {
+            avformat_close_input(&avctx);
+            return NULL;
+        }
+    }
+
+    struct MediaInfo *media_info = calloc(1, sizeof(struct MediaInfo));
+    memset(media_info, 0, sizeof(struct MediaInfo));
+
+    struct TrackInfo *track_cur = NULL;
+    struct ImageInfo *image_cur = NULL;
+
+    // av_dump_format(avctx, 0, NULL, 0);
+
+    for (unsigned int i = 0; i < avctx->nb_streams; ++i) {
+        struct TrackInfo *track_info = try_get_track_info(avctx, i, 0, path);
+        if (track_info) {
+            if (!track_cur) {
+                media_info->tracks = track_info;
+            } else {
+                track_cur->next = track_info;
+            }
+
+            track_cur = track_info;
+
+            continue;
+        }
+
+        struct ImageInfo *image_info = try_get_image_info(avctx, i, path);
+        if (image_info) {
+            if (!image_cur) {
+                media_info->images = image_info;
+            } else {
+                image_cur->next = image_info;
