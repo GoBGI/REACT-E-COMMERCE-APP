722f0a72ec89d479c9c838703669cefc7d06e68b
GoBGI <88916280+GoBGI@users.noreply.github.com>
2023-01-31 08:26:19 +0000

    add usage to README

--- src/musicd.c
+++ src/musicd.c
@@ -2,4 +2,7 @@
 
 static void (*log_callback)(int level, const char *);
 
-static void lav_callback(void *av_class, int av_level, const char *fmt, va_list v
+static void lav_callback(void *av_class, int av_level, const char *fmt, va_list va_args) {
+    (void)av_class;
+
+    int level = 
