784d45487fdacadc9b96ce3d8641af0a26a2abb1
GoBGI <88916280+GoBGI@users.noreply.github.com>
2023-02-28 05:51:30 +0000

    Set next development version

--- src/scan.rs
+++ src/scan.rs
@@ -17,3 +17,62 @@ use crate::media;
 #[derive(Debug)]
 pub enum Error {
     IoError(std::io::Error),
+    DatabaseError(rusqlite::Error),
+    OtherError,
+}
+
+impl From<std::io::Error> for Error {
+    fn from(err: std::io::Error) -> Error {
+        Error::IoError(err)
+    }
+}
+
+impl From<rusqlite::Error> for Error {
+    fn from(err: rusqlite::Error) -> Error {
+        Error::DatabaseError(err)
+    }
+}
+
+impl std::fmt::Display for Error {
+    fn fmt(&self, f: &mut std::fmt::Formatter) -> std::fmt::Result {
+        write!(f, "{}", self.description())
+    }
+}
+
+impl StdError for Error {
+    fn description(&self) -> &str {
+        match *self {
+            Error::IoError(ref e) => e.description(),
+            Error::DatabaseError(ref e) => e.description(),
+            Error::OtherError => "Other error",
+        }
+    }
+}
+
+pub type Result<T> = std::result::Result<T, Error>;
+
+pub struct ScanThread {
+    stop: Arc<AtomicBool>,
+    join_handle: Mutex<Option<JoinHandle<ScanStat>>>,
+}
+
+impl ScanThread {
+    pub fn new() -> ScanThread {
+        ScanThread {
+            stop: Arc::new(AtomicBool::new(false)),
+            join_handle: Mutex::new(None),
+        }
+    }
+
+    pub fn is_running(&self) -> bool {
+        self.join_handle.lock().unwrap().is_some()
+    }
+
+    pub fn start(&self, index: Index) {
+        {
+            if self.join_handle.lock().unwrap().is_some() {
+                return;
+            }
+        }
+
+        let stop = self.stop.clone();
