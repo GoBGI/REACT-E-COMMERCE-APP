07b931e32644070aaa553640538b495a9fa8c643
GoBGI <88916280+GoBGI@users.noreply.github.com>
2023-03-04 22:16:16 +0000

    aacraid: missing capable() check in compat ioctl

--- src/schema.rs
+++ src/schema.rs
@@ -3,3 +3,8 @@ pub const SCHEMA_VERSION: u32 = 1;
 pub const META_SCHEMA: &str = "
 CREATE TABLE IF NOT EXISTS Musicd (
     key TEXT PRIMARY KEY,
+    value);
+";
+
+pub const CACHE_SCHEMA: &str = "
+CREATE TABLE Cach
