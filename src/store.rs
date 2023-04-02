330bf7b931dc4759370550a760739526483b18b3
GoBGI <88916280+GoBGI@users.noreply.github.com>
2023-04-02 20:53:05 +0000

    initialize project

--- src/store.rs
+++ src/store.rs
@@ -53,3 +53,69 @@ impl StoreSource {
                 );
                 return Err(e);
             }
+        };
+
+        conn.execute_batch(
+            "PRAGMA foreign_keys = ON;
+            PRAGMA journal_mode = WAL;",
+        )?;
+
+        Ok(Store { conn, index })
+    }
+}
+
+impl Store {
+    pub fn synchronize(&mut self) -> Result<()> {
+        debug!("synchronize");
+
+        let store_conn = &self.conn;
+        let index_conn = self.index.connection();
+
+        index_conn.execute_batch(
+            "DELETE FROM StoreListTrack;
+            DELETE FROM StoreList;
+            DELETE FROM StoreTrack;",
+        )?;
+
+        let mut st = store_conn.prepare(
+            "SELECT store_track_id, title, artist_name, album_name, length, play_count, last_play
+            FROM Track",
+        )?;
+
+        let mut rows = st.query(NO_PARAMS)?;
+
+        let mut st = index_conn.prepare(
+            "INSERT OR IGNORE INTO
+                StoreTrack (track_id, store_track_id, play_count, last_play)
+            VALUES
+                (
+                    (
+                        SELECT track_id
+                        FROM Track
+                        WHERE
+                            Track.title = ? AND
+                            Track.artist_name = ? AND
+                            Track.album_name = ?
+                    ),
+                    ?,
+                    ?,
+                    ?
+                )",
+        )?;
+
+        while let Some(row) = rows.next()? {
+            let store_track = StoreTrack {
+                store_track_id: row.get(0)?,
+                title: row.get(1)?,
+                artist_name: row.get(2)?,
+                album_name: row.get(3)?,
+                length: row.get(4)?,
+                play_count: row.get(5)?,
+                last_play: row.get(6)?,
+            };
+
+            trace!("add to index {:?}", store_track);
+
+            st.execute(params![
+                store_track.title,
+                store_track.artist_name,
